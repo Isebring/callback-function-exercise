112599b6239e4a26d9e204514cda032a74de50d4
Hampus Isebring <hampus.isebring@gmail.com>
2022-11-15 10:40:50 +0100

    added interval function to generate different bgcolors

--- main.js
+++ main.js
@@ -9,15 +9,39 @@ function main() {
 }
 
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = 'lightblue';
+    document.body.style.backgroundColor = getRandomColor();
 }
 
 function addEventListeners() {
 
-const button = document.getElementById('cancel');
-  button.addEventListener('click', cancelBackgroundChange); 
+const cancelTimeoutbutton = document.getElementById('cancel');
+  cancelTimeoutbutton.addEventListener('click', cancelBackgroundChange); 
+
+const startIntervalButton = document.getElementById('start-interval');
+ startIntervalButton.addEventListener('click', startBackgroundChangeInterval);
+
 }
 
 function cancelBackgroundChange() {
 clearTimeout(timeoutRef);
+}
+
+function startBackgroundChangeInterval() {
+    setInterval(changeBackgroundColor, 1000)
+}
+
+function getRandomColor() {
+    const value = Math.random();
+    
+    if (value < 0.2) {
+    return 'blue';
+    } else if (value < 0.4) {
+        return 'red';
+    } else if (value < 0.6) {
+        return 'green';
+    } else if (value < 0.8) {
+        return 'grey';
+    } else {
+        return 'pink';
+    } 
 }
